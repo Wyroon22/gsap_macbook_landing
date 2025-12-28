8af6a8415635fbc87f2fd2cb3ccf4214fa111f58
Weeratat Kwandee <windyman18@gmail.com>
2025-12-28 12:28:36 +0700

    Implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero.jsx";
 
 
 const App = () => {
     return (
         <>
             <NavBar />
+            <Hero />
 
         </>
     );
