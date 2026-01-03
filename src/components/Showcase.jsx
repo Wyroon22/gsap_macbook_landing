a3e14ec2189bf4c2e44a6a40416aa6054fa1252a
Weeratat Kwandee <windyman18@gmail.com>
2026-01-03 19:42:44 +0700

    implement section

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -2,6 +2,7 @@ import {useMediaQuery} from "react-responsive";
 import {useGSAP} from "@gsap/react";
 import gsap from 'gsap';
 
+
 const Showcase = () => {
     const isTablet = useMediaQuery({query: '(max-width: 1024px)'});
 
